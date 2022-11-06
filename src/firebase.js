daf2925ad78a135985799b99a93b3efa32fc6007
Dhruv Gupta <dhruv.gupta1873@gmail.com>
2022-11-07 00:16:01 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9aRzShARf-09dEFRZkZCcwUuYBFCaE04",
+    authDomain: "netflix-clone-a49e4.firebaseapp.com",
+    projectId: "netflix-clone-a49e4",
+    storageBucket: "netflix-clone-a49e4.appspot.com",
+    messagingSenderId: "347734210163",
+    appId: "1:347734210163:web:64889170040b296bbc5c06",
+    measurementId: "G-MW9BSP62TM"
+  };
