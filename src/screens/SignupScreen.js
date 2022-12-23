65580cf8ae15d059159bcf8363475fbcf8ea3684
Dhruv Gupta <dhruv.gupta1873@gmail.com>
2022-12-23 21:07:34 +0530

    commited unstaged

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import "./SignupScreen.css";
 
 function SignupScreen() {
   return (
-    <div>SignupScreen</div>
+    <div className='signupScreen'>SignupScreen
+    </div>
   )
 }
 
