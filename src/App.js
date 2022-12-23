a9dac18416334d760fc5bbdae5bca81d6fc8f56b
Dhruv Gupta <dhruv.gupta1873@gmail.com>
2022-12-23 21:06:36 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="App">
-      <h1>Lets build Netflix</h1>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
